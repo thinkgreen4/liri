fed6e964ccc0e76f4df7ca2b98f4bb0ce395a47f
thinkgreen4 <thinkgreen4@hotmail.com>
2018-06-28 15:05:53 -0600

    push

--- keys.js
+++ keys.js
@@ -1,13 +1,17 @@
+console.log('this is loaded');
 
-exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-};
-
-exports.spotify = {
-  SPOTIFY_ID: process.env.SPOTIFY_ID,
+module.exports = {
+twitter: {
+  
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  
+},
+spotify: {
+  id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
-};
+}
 //spotfiy 
+};
--- liri.js
+++ liri.js
@@ -4,31 +4,58 @@ var keys = require('./keys.js');
 
 var request = require('request');
 
-var twitter = require('twitter');
+var Twitter = require('twitter');
 
-var spotify = require('node-spotify-api');
+var Spotify = require('node-spotify-api');
 
 var request = require("request");
 
 var fs = require('fs');
 
 
+console.log(process.argv);
+
+ function getMovieName() {
+     var movieName = ''
+     for(var i = 3; i < process.argv.length; i++){
+         console.log(process.argv[i]);
+         movieName = movieName + process.argv[i] +''
+
+     }
+     console.log(movieName);
+     return movieName
+ }
+ nodeArgv = process.argv;
+var x = "";
+//attaches multiple word arguments
+for (var i=3; i<nodeArgv.length; i++){
+  if(i>3 && i<nodeArgv.length){
+    x = x + "+" + nodeArgv[i];
+  } else{
+    x = x + nodeArgv[i];
+  }
+}
 
 var action = process.argv[2];
 var value = process.argv[3];
 
 
 
-switch (action, value) {
+switch (action) {
     case "my-tweets":
         twatter();
+        console.log("tweets")
         break;
     case "spotify-this-song":
-        song();
-        break;
+     if(x){
+        track(x);   
+     } else 
+     {track("weston road flows");
+    }
+     break;
     case "movie-this":
-        film();
-        break;
+     film();
+    break;
     case "do-what-it-says":
         text();
         break;
@@ -47,16 +74,22 @@ switch (action, value) {
 //The album that the song is from
 function twatter() {
     var client = new Twitter(keys.twitter);
-
+    var params = {screen_name: 'billy'};
     client.get('statuses/user_timeline', function(error, tweets, response) {
         if(error) throw error;
         
-        for (var i =0; i < tweets.length;i++)
-        var recent = "Tweet " + (i+1) + ": " + '\n' + tweets[i].text;
+        for (var i =0; i < tweets.length;i++){
+            var recent = "Tweet " + (i+1) + ": " + '\n' + tweets[i].text;
 
-        console.log("my sweeet ass tweets yo" + recent)
-        console.log("Tweeted at:  " + tweets[i].created_at);
-        console.log("");
+            console.log("my sweeet ass tweets yo" + recent)
+            console.log("Tweeted at:  " + tweets[i].created_at);
+            console.log("");
+            // apend file to random.txt
+            // fs.appendFile("random.txt","my sweeet ass tweets yo" + recent)
+            // fs.appendFile("random.txt","Tweeted at:  " + tweets[i].created_at);
+            // fs.appendFile("");
+        }
+        
 
         
       });
@@ -74,17 +107,34 @@ function twatter() {
 //Step Two: Either login to your existing Spotify account or create a new one (a free account is fine) and log in.
 //Step Three: Once logged in, navigate to https://developer.spotify.com/my-applications/#!/applications/create to register a new application to be used with the Spotify API. You can fill in whatever you'd like for these fields. When finished, click the "complete" button.
 //Step Four: On the next screen, scroll down to where you see your client id and client secret. Copy these values down somewhere, you'll need them to use the Spotify API and the node-spotify-api package.
-function song(song) {
+function track(song) {
     var spotify = new Spotify(keys.spotify);
-    spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
-        if (err) {
-          return console.log('Error occurred: ' + err);
+    spotify.search({ type: 'track', query: song}, function(err, data) {
+        if (!err) {
+       
+            var songData = data.tracks.items[0];
+            //artist
+            console.log("Artist: " + songData.artists[0].name);
+            //song name
+            console.log("Song: " + songData.name);
+            // //spotify preview link
+             console.log("Preview URL: " + songData.preview_url);
+            // //album name
+             console.log("Album: " + songData.album.name);
+            console.log("-----------------------");
+
+            //append file to random.txt
+            // fs.appendFile('random.txt', songData.artists[0].name);
+            // fs.appendFile('random.txt', songData.name);
+            // fs.appendFile('random.txt', songData.preview_url);
+            // fs.appendFile('random.txt', songData.album.name);
+            // fs.appendFile('random.txt', "-----------------------");
+
+          }
+         else{
+            return console.log('Error occurred: ' + err);
         }
        
-      console.log(data); 
-      console.log("artists" + data.artist);
-      console.log("artists" + data.artist);
-      console.log("artists" + data.artist);
       });
 
 
@@ -103,25 +153,38 @@ function song(song) {
 
 function film(movie) {
     
-var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+var queryUrl = "http://www.omdbapi.com/?t=" + getMovieName() + "&y=&plot=short&apikey=trilogy";
 
     request(queryUrl, function(error, response, body) {
 
-        // If the request is successful
+        //If the request is successful
         if (!error && response.statusCode === 200) {
       
-          // Parse the body of the site and recover just the imdbRating
-          // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-          console.log("Release Year: " + JSON.parse(body).Title);
+         // Parse the body of the site and recover just the imdbRating
+         // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+          console.log("Title: " + JSON.parse(body).Title);
           console.log("Release Year: " + JSON.parse(body).Year);
-          console.log("Release Year: " + JSON.parse(body).imdbRating);
-          // figure out how to get parameter to filter out rotten tomatoes rating
-         console.log("Release Year: " + JSON.parse(body).body.tomatoRating);
-          console.log("Release Year: " + JSON.parse(body).Country);
-          console.log("Release Year: " + JSON.parse(body).Language);
-          console.log("Release Year: " + JSON.parse(body).Plot);
+          console.log("Rating: " + JSON.parse(body).imdbRating);
+         // figure out how to get parameter to filter out rotten tomatoes rating
+        
+          console.log("Country: " + JSON.parse(body).Country);
+          console.log("Language: " + JSON.parse(body).Language);
+          console.log("Plot: " + JSON.parse(body).Plot);
 
-          console.log("Release Year: " + JSON.parse(body).Actors);
+          console.log("Actors: " + JSON.parse(body).Actors);
+
+          // append file ro random.txt
+        //   fs.appendFile("random.txt", "Title: " + JSON.parse(body).Title);
+        //   fs.appendFile("random.txt","Release Year: " + JSON.parse(body).Year);
+        //   fs.appendFile("random.txt","Rating: " + JSON.parse(body).imdbRating);
+        //  // figure out how to get parameter to filter out rotten tomatoes rating
+        
+        //   fs.appendFile("random.txt","Country: " + JSON.parse(body).Country);
+        //   fs.appendFile("random.txt","Language: " + JSON.parse(body).Language);
+        //   fs.appendFile("random.txt","Plot: " + JSON.parse(body).Plot);
+
+        //   fs.appendFile("random.txt","Actors: " + JSON.parse(body).Actors);
+          
         }
       });
       
@@ -129,13 +192,21 @@ var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey
     }
 
     function text(){
-        var spotify = new Spotify(keys.spotify);
-        fs.readFile('random.txt', "utf8", function(error, data){
-          var txt = data.split(',');
-      
-          song(txt[1]);
-        });
-      }
+        
+            fs.readFile("random.txt", "utf8", function(error, data){
+                if (error) {
+                    return console.log(error);
+                }
+        
+                var randomChoice = data.split(",");
+                console.log(randomChoice);
+                if(randomChoice[0] === "spotify-this-song") {
+                    spotifyThis(randomChoice[1]);
+                }
+                
+            })
+        };
+        
 
 //node liri.js do-what-it-says Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
 //It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
